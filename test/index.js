45a0c998424f9b37b9e059dcac7e4b27a93dbd0f
zensh <admin@zensh.com>
2015-06-28 09:41:38 +0800

    add event "ssh2Data", use standard style

--- test/index.js
+++ test/index.js
@@ -1,12 +1,10 @@
-'use strict';
+'use strict'
 
-var gulp = require('gulp');
-var GulpSSH = require('../index');
-var gulpSequence = require('gulp-sequence');
-var through = require('through2');
+var gulp = require('gulp')
+var GulpSSH = require('../index')
+var gulpSequence = require('gulp-sequence')
 
-module.exports = function() {
-
+module.exports = function () {
   var gulpSSH = new GulpSSH({
     ignoreErrors: false,
     sshConfig: {
@@ -15,30 +13,30 @@ module.exports = function() {
       username: 'root',
       privateKey: require('fs').readFileSync('/Users/zensh/.ssh/id_rsa')
     }
-  });
+  })
 
-  gulp.task('exec', function() {
+  gulp.task('exec', function () {
     return gulpSSH
       .exec(['uptime', 'ls -a', 'pwd'], {filePath: 'commands.log'})
-      .pipe(gulp.dest('logs'));
-  });
+      .pipe(gulp.dest('logs'))
+  })
 
-  gulp.task('sftp-read', function() {
+  gulp.task('sftp-read', function () {
     return gulpSSH.sftp('read', 'install.log')
-      .pipe(gulp.dest('logs'));
-  });
+      .pipe(gulp.dest('logs'))
+  })
 
-  gulp.task('sftp-write', function() {
+  gulp.task('sftp-write', function () {
     return gulp.src('index.js')
-      .pipe(gulpSSH.sftp('write', 'test.js'));
-  });
+      .pipe(gulpSSH.sftp('write', 'test.js'))
+  })
 
-  gulp.task('shell', function() {
+  gulp.task('shell', function () {
     return gulpSSH
       .shell(['cd /home/thunks', 'git pull', 'npm install', 'npm update', 'npm test'], {filePath: 'shell.log'})
-      .pipe(gulp.dest('logs'));
-  });
+      .pipe(gulp.dest('logs'))
+  })
 
-  gulp.task('test', gulpSequence('exec', 'sftp-read', 'sftp-write', 'shell'));
+  gulp.task('test', gulpSequence('exec', 'sftp-read', 'sftp-write', 'shell'))
 
-};
+}
