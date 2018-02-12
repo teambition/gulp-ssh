1f6e395eb3029465631ce90f275245f9f1829d9b
Dan Allen <dan.j.allen@gmail.com>
2018-02-11 23:59:41 -0700

    remove gulp build and samples

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-var gulp = require('gulp')
-var samples = require('./test/samples')
-var gulpSequence = require('gulp-sequence')
-
-samples()
-
-gulp.task('default', gulpSequence('samples'))
